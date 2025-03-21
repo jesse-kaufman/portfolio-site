7f2f3e09906e21bf475c22781df50a70813242a2
jesse-kaufman <jesse@jessekaufman.com>
2025-03-20 22:23:04 -0500

    refactor: move sending mail to separate service

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,5 @@
 import cors from "cors"
 import express from "express"
-import nodemailer from "nodemailer"
 import chalk from "chalk"
 import { checkEnv } from "./services/checkEnv.js"
 
@@ -33,47 +32,17 @@ app.use(express.urlencoded({ extended: true }))
 
 // API endpoint to handle contact form submissions
 app.post("/contact/v1", (req, res) => {
-  // Configure nodemailer transport with encryption (SSL or TLS)
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_SERVER, // Your local mail server address (use 'localhost' for local)
-    port: process.env.SMTP_PORT, // Port for SSL (usually 465 or 587 for TLS)
-    secure: true, // Use SSL/TLS
-    auth: {
-      user: "", // Leave empty if no authentication is required
-      pass: "", // Leave empty if no authentication is required
-    },
-  })
-
   const { name, email, phone, subject, message } = req.body
 
-  // Input validation
-  if (!name || !email || !phone || !subject || !message) {
-    return res.status(400).json({ error: "All fields are required." })
+  try {
+    sendMail({ name, email, phone, subject, message })
+  } catch (error) {
+    console.log(chalk.red("Error:", error))
+
+    return res
+      .status(500)
+      .json({ error: "Something went wrong. Please try again later." })
   }
-
-  // Setup email data
-  const mailOptions = {
-    to: "youremail@example.com", // Recipient address
-    subject: subject, // Subject line
-    text: `Message from ${name} (${email}):\n\nPhone: ${phone}\n\n${message}`, // Plain text body
-    html: `<p>Message from ${name} (${email}):</p><p><strong>Phone:</strong> ${phone}</p><p><strong>Message:</strong><br/>${message}</p>`, // HTML body
-    replyTo: email, // Use sender email for reply-to header
-  }
-
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log("Error:", error)
-      return res
-        .status(500)
-        .json({ error: "Something went wrong. Please try again later." })
-    }
-
-    console.log("Email sent:", info.response)
-    res
-      .status(200)
-      .json({ message: "Your message has been sent successfully!" })
-  })
 })
 
 // Start the server
