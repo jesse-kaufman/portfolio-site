34e9cc833cd36205fe967dc68dd918236c2bb80b
jesse-kaufman <jesse@jessekaufman.com>
2025-03-25 17:03:42 -0500

    feat: add initial ContactForm and InputText components

--- src/contact-form/components/ContactForm.vue
+++ src/contact-form/components/ContactForm.vue
@@ -1,14 +1,31 @@
-<script setup>
-import { ref } from "vue"
-const count = ref(0)
-</script>
-
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <form @submit.prevent="handleSubmit">
+    <TextInput
+      v-model="formData.name"
+      label="Your Name"
+      name="name"
+      type="text"
+      required
+    />
+    <button type="submit" disabled>Submit</button>
+  </form>
 </template>
 
+<script setup>
+import { ref } from "vue"
+import TextInput from "./TextInput.vue"
+
+const formData = ref({
+  name: "",
+})
+
+const handleSubmit = () => {
+  console.log("Form submitted with data:", formData.value)
+}
+</script>
+
 <style scoped>
-button {
+label {
   font-weight: bold;
 }
 </style>
---
+++ src/contact-form/components/TextInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <label :for="name">{{ label }}</label>
+    <input
+      :id="name"
+      v-model="inputValue"
+      v-bind="$attrs"
+      :name="name"
+      :type="type"
+      @input="updateValue"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  label: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "",
+  },
+})
+
+const emit = defineEmits(["update:modelValue"])
+const inputValue = ref(props.modelValue)
+
+const updateValue = (event) => {
+  inputValue.value = event.target.value
+  emit("update:modelValue", inputValue.value)
+}
+</script>
