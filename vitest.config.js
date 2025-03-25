02d5213819f40882052086fc5d99ba0b5b7203f8
jesse-kaufman <jesse@jessekaufman.com>
2025-03-25 14:53:04 -0500

    test: add jsdom for access to document inside tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vitest/config"
 import path from "path"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   test: {
-    // Other Vitest options
+    environment: "jsdom", // This ensures jsdom is used in your tests
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Ensure this resolves to /src
     },
   },
+  plugins: [vue()],
 })
